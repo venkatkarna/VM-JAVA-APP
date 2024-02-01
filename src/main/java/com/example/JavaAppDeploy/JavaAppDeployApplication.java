00969840e2bd01da2abb6cffb9c38052aa229dad
venkatesh.b <venkatesh.b@changepond.com>
2024-02-01 13:13:10 +0530

    first commit

---
+++ src/main/java/com/example/JavaAppDeploy/JavaAppDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaAppDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAppDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaAppDeploy/JavaAppDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaAppDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
